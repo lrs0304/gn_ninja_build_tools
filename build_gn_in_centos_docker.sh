d4ea772ee01550faf0cbdb9ba8619bc4eefb86bc
risonliang <risonliang@tencent.com>
2023-04-07 16:54:41 +0800

    检查gcc版本后再进行安装gcc，避免重复安装

--- build_gn_in_centos_docker.sh
+++ build_gn_in_centos_docker.sh
@@ -11,15 +11,23 @@ chmod +x ${script_path}/build_gn.sh
 echo "install ninja"
 yum -y install ninja-build
 
-echo "install gcc11"
-yum -y install centos-release-scl
+echo "test gcc whether large than 10 or install gcc11"
+gcc --version
+gcc --version | grep -q "gcc (.*[[:space:]])\?10\."
+if [ $? -eq 0 ]; then
+	bash ${script_path}/build_gn.sh
+else
+	echo "current GCC version is less than 10"
+fi
+
+echo "try install gcc-toolset-11"
+yum -y install gcc-toolset-11
 if [[ "$?" -eq "0" ]];then
+	yum -y install centos-release-scl
+	scl enable gcc-toolset-11 ${script_path}/build_gn.sh
+else
 	echo "support centos-release-scl"
 	yum -y install scl-utils
 	yum -y install devtoolset-11-gcc devtoolset-11-gcc-c++ devtoolset-11-binutils
 	scl enable devtoolset-11 ${script_path}/build_gn.sh
-else
-	echo "try install gcc-toolset-11"
-	yum -y install gcc-toolset-11
-	scl enable gcc-toolset-11 ${script_path}/build_gn.sh
 fi
